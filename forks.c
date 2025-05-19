d5aef9a69b66e397f6f2c45172dbf6ef3466aeb5
murilo-henrique060 <henrique.henryconde@gmail.com>
2025-05-18 23:12:53 -0300

    Fix loop index in forks.c to correctly iterate from 1 to n

--- forks.c
+++ forks.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 
     sscanf(argv[1], "%d", &n);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         pid = fork();
         
         if (pid < 0) {
