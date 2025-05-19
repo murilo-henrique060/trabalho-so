072b18cb984722f87ed9424b2fb87763d6c80a58
murilo-henrique060 <henrique.henryconde@gmail.com>
2025-05-18 23:11:31 -0300

    Refactor fork handling in forks.c to improve error management and clarity

--- forks.c
+++ forks.c
@@ -4,20 +4,20 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    int i = 0, pid;
+    int n, pid;
 
-    while (pid > 0) {
+    sscanf(argv[1], "%d", &n);
+
+    for (int i = 0; i < n; i++) {
         pid = fork();
         
-        if (pid == 0) {
+        if (pid < 0) {
+            while (wait(NULL) > 0);
+
+            printf("Fork failed on: %d\n", i);
+            exit(1);
+        } else if (pid == 0) {
             execl("helloworld", "helloworld", NULL);
         }
-
-        i++;
     }
-
-    while (wait(NULL) > 0);
-    
-    printf("Number of forks: %d\n", i);
-    return 0;
 }
